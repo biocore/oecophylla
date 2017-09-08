9a1b6078c37c7eeede708ffe26603e8b3c416d40
Jon Sanders <jsanders@oeb.harvard.edu>
2017-09-07 17:37:47 -0700

    updated README and base conda env name

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 # runs setup of base environment and database installs
 
 # create conda env
-conda env create --name ratsnake_shotgun -f environment.yml
+conda env create --name oecophylla -f environment.yml
+
+conda env create --name shotgun-humann2 -f envs/shotgun-humann2.yaml
+conda env create --name shotgun-qc -f envs/shotgun-qc.yaml
+conda env create --name shotgun-shogun -f envs/shotgun-shogun.yaml
 
 # download UTree binary and add to path
 wget https://github.com/knights-lab/UTree/releases/download/v1.2/utree_1.2_linux.zip
