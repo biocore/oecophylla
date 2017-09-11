07fd87fe2b5dccb5012270924b6aa8f3c6fc7e56
Jon Sanders <jsanders@oeb.harvard.edu>
2017-09-11 16:18:39 -0700

    echo conda prefix

--- envs/shotgun-shogun.sh
+++ envs/shotgun-shogun.sh
@@ -1,6 +1,8 @@
 conda env create -n shotgun-shogun -f shotgun-shogun.yaml
 source activate shotgun-shogun
 
+echo $CONDA_PREFIX
+
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 mkdir -p $CONDA_PREFIX/etc/conda/deactivate.d
 
