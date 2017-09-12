e44198bf7426f33a02e73224231587394e1e7605
mortonjt <jamietmorton@gmail.com>
2017-09-12 15:26:18 -0700

    fixing install

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # runs setup of base environment and database installs
 
 # create core conda env
-# conda env create --name oecophylla -f environment.yml
+conda env create --name oecophylla -f environment.yml
 
 find oecophylla -name "*.sh" -execdir bash {} \;
 
