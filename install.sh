2c4d7430e59056c593044d76b03e02364185dcb2
Jon Sanders <jsanders@oeb.harvard.edu>
2017-09-11 17:57:14 -0700

    simplifying installs for current working modules

--- install.sh
+++ install.sh
@@ -5,17 +5,17 @@
 # create conda env
 conda env create --name oecophylla -f environment.yml
 
-conda env create --name shotgun-humann2 -f envs/shotgun-humann2.yaml
+# conda env create --name shotgun-humann2 -f envs/shotgun-humann2.yaml
 conda env create --name shotgun-qc -f envs/shotgun-qc.yaml
 # conda env create --name shotgun-shogun -f envs/shotgun-shogun.yaml
 # currently shogun is a hack, running the install script until we 
 # have stable conda install
-bash envs/shotgun-shogun.sh
+# bash envs/shotgun-shogun.sh
 
-# download UTree binary and add to path
-wget https://github.com/knights-lab/UTree/releases/download/v1.2/utree_1.2_linux.zip
-unzip utree_1.2_linux.zip -d utree
-chmod 755 utree/utree*
+# # download UTree binary and add to path
+# wget https://github.com/knights-lab/UTree/releases/download/v1.2/utree_1.2_linux.zip
+# unzip utree_1.2_linux.zip -d utree
+# chmod 755 utree/utree*
 
 # unzip test db
-bunzip2 test_data/shogun_test_db/phix.ctr.bz2
+# bunzip2 test_data/shogun_test_db/phix.ctr.bz2
