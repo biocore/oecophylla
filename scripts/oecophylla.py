f4bce9fb8d1eb3c41894ae36133bfe342c20d086
tkosciol <tkosciolek@ucsd.edu>
2017-09-14 11:57:48 -0700

    minor updates to launch script

--- scripts/oecophylla.py
+++ scripts/oecophylla.py
@@ -1,17 +1,14 @@
 import click
+import json
+import os
+
 """
-notes
------
+This is the main Oecophylla script which handles launching of the entire
+pipeline.
 
-we need to activate the environment first
 
-expected functionality:
-  * torque submission
-  * slurm submission (flag --cluster-queue)
-  * local run (flag --cluster-queue local)
-
-  * option to generate only config files (flag)
 """
+
 
 @click.group()
 def run():
@@ -24,71 +21,80 @@ def _arg_split(ctx, param, value):
     return _files
 
 @run.command()
-@click.option('--input-dir', required=True, type=click.STRING,
+@click.option('--input-dir', '-i', required=True, type=click.STRING,
               callback=_arg_split,
-              help='Input directories of all of the samples.')
-@click.option('--sample-sheet', required=True, type=click.STRING,
+              help='Input directory with all of the samples.')
+@click.option('--sample-sheet', '-s', required=True, type=click.STRING,
               callback=_arg_split,
-              help='Sample sheets used to demultiplex the illumina run.')
-@click.option('--params', type=click.PATH, required=True,
+              help='Sample sheets used to demultiplex the Illumina run.')
+@click.option('--params', '-p', type=click.PATH, required=True,
               help='Specify parameters for the tools.')
-@click.option('--local-scratch', type=click.PATH, required=False,
+@click.option('--cluster-params', type=click.PATH, required=False,
+              help='File with parameters for a cluster job.')
+@click.option('--local-scratch', type=click.PATH, default='/tmp'
+              help='Temporary directory for storing intermediate files.')
+@click.option('--run-location',
+              type=click.Choice(['torque', 'slurm', 'local']),
+              default='local',
               help='Temporary directory for storing intermediate files.')
 @click.option('--log-dir', type=click.PATH, required=False,
+              default='/dev/null',
               help='Directory containing log files.')
+@click.option('--output-dir', '-o', type=click.PATH, required=True,
+              help='Input directory of all of the samples.')
 @click.option('--restart', is_flag=True, default=False,
               help='Restarts the run and overwrites previous input.')
-@click.option('--output-dir', type=click.PATH, required=False,
-              help='Input directory of all of the samples.')
 # TODO: extra snakemake parameters with defaults
-
 # TODO: everything past here will be passed directly into snakemake
-def local_workflow():
-    """
+def workflow():
+    import snakemake
+    from skbio.io.registry import Format
 
-    If a manifest is not specified, the files containing forward and reverse reads
-    will be automatically identified using regex.
+    # INPUT DIR
+    # sniff with scikit bio
 
-    """
+    # SAMPLE MANIFEST
+    # If a manifest is not specified, the files containing forward and reverse reads
+    # will be automatically identified using regex.
 
+
+    # OUTPUT
+
+
+    # LOGS
+    if log_dir:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    else:
+        os.makedirs('%s/%s' % (output_dir, 'cluster_logs'))
+
+
+    snakefile = "%s/../Snakefile" % __file__
+    with open(cluster_params) as _file:
+        cluster = json.load(_file)
+    # TODO cluster queue logic
+
+    snakemake.snakemake(snakefile, listrules=False, list_target_rules=False,
+                        cores=cluster['cores'],
+                        nodes=cluster['nodes'],
+                        local_cores=cluster['local_cores'],
+                        resources={},
+                        config={}, configfile=None, config_args=None,
+                        workdir=None, targets=None, dryrun=False, touch=False,
+                        forcetargets=False, forceall=False, forcerun=[],
+                        until=[], omit_from=[], prioritytargets=[], stats=None,
+                        printreason=False, printshellcmds=False,
+                        debug_dag=False)
     # --no-lock?
 
     # parameters, samples and environmental variables will all have to be concatenated
     # together to feed into snakemake.
-    pass
 
-@run.command()
-@click.option('--input-dir', required=True, type=click.STRING,
-              callback=_arg_split,
-              help='Input directories of all of the samples.')
-@click.option('--sample-sheet', required=True, type=click.STRING,
-              callback=_arg_split,
-              help='Sample sheets used to demultiplex the illumina run.')
-@click.option('--params', type=click.PATH, required=True,
-              help='Specify parameters for the tools.')
-@click.option('--cluster-params', type=click.PATH, required=True,
-              help='Input biom table of abundances.')
-@click.option('--local-scratch',
-              help='Temporary directory for storing intermediate files.')
-@click.option('--qsub/--slurm', is_flag=True,
-              help='Temporary directory for storing intermediate files.')
-@click.option('--log-dir', type=click.PATH, required=False,
-              help='Directory containing log files.')
-@click.option('--output-dir', type=click.PATH, required=False,
-              help='Input directory of all of the samples.')
-def cluster_workflow():
-    """
-
-    If a manifest is not specified, the files containing forward and reverse reads
-    will be automatically identified using regex.
-
-    """
 
     # below are some examples of cluster inputs
     # -j 16 --local-cores 4 -w 90 --cluster-config cluster.json
     # --cluster "qsub -e {cluster.error} -o {cluster.output} -m n -l nodes=1:ppn={cluster.n} -l mem={cluster.mem}gb -l walltime={cluster.time}"
 
-    pass
 
 @run.command()
 @click.option('--config-file',
---
+++ scripts/tests/test_oecophylla.py
@@ -0,0 +1,80 @@
+from unittest import TestCase, main
+from click.testing import CliRunner
+from shutil import rmtree
+from os import remove
+from os.path import join, basename
+from tempfile import mkdtemp
+from skbio.util import get_data_path
+from skbio import Sequence
+from glob import glob
+
+class ProcessingTests(TestCase):
+    def setUp(self):
+        # temporary working directory
+        self.working_dir = mkdtemp()
+
+        # test data files
+        dir = 'test_process_fasta'
+        self.input_faa = get_data_path(join(dir, 'input.faa'))
+        self.represent = get_data_path(join(dir, 'represent.xml'))
+        self.pdb_seqres = get_data_path(join(dir, 'pdb_seqres.txt'))
+
+        self.split_1 = get_data_path(join(dir, '1K5N_B.fasta'))
+        self.split_2 = get_data_path(join(dir, '2VB1_A.fasta'))
+        self.split_3 = get_data_path(join(dir, '3J4F_A.fasta'))
+
+        # test protein sequences
+        self.seqs = []
+        x = Sequence('PIVQNLQGQMVHQAISPRTLNAWVKVVEEKAFSPEVIPMFSALSEGATPQDLNTML'
+                     'NTVGGHQAAMQMLKETINEEAAEWDRLHPVHAGPIEPGQMREPRGSDIAGTTSTLQ'
+                     'EQIGWMTHNPPIPVGEIYKRWIILGLNKIVRMYSPTSILDIRQGPKEPFRDYVDRF'
+                     'YKTLRAEQASQEVKNWMTETLLVQNANPDCKTILKALGPAATLEEMMTACQGVGGP'
+                     'GHKARVL')
+        x.metadata['id'] = '3J4F_A'
+        x.metadata['description'] = ('Chain A, Structure Of Hiv-1 Capsid Prote'
+                                     'in By Cryo-em')
+        self.seqs.append(x)
+        x = Sequence('MIQRTPKIQVYSRHPAENGKSNFLNCYVSGFHPSDIEVDLLKNGERIEKVEHSDLS'
+                     'FSKDWSFYLLYYTEFTPTEKDEYACRVNHVTLSQPKIVKWDRDM')
+        x.metadata['id'] = '1K5N_B'
+        x.metadata['description'] = ('Chain B, Hla-B2709 Bound To Nona-Peptide'
+                                     ' M9')
+        self.seqs.append(x)
+        x = Sequence('KVFGRCELAAAMKRHGLDNYRGYSLGNWVCAAKFESNFNTQATNRNTDGSTDYGIL'
+                     'QINSRWWCNDGRTPGSRNLCNIPCSALLSSDITASVNCAKKIVSDGNGMNAWVAWR'
+                     'NRCKGTDVQAWIRGCRL')
+        x.metadata['id'] = '2VB1_A'
+        x.metadata['description'] = ('Chain A, Hewl At 0.65 Angstrom Resolutio'
+                                     'n')
+        self.seqs.append(x)
+
+    def test_workflow(self):
+        # get representative proteins
+        params = ['--infile', self.input_faa,
+                  '--outfile', join(self.working_dir, 'output.faa'),
+                  '--identifiers', None,
+                  '--represent', self.represent]
+        res = CliRunner().invoke(_process_fasta, params)
+        self.assertEqual(res.exit_code, 0)
+        exp = ('Number of representative proteins: 3\n'
+               'Number of extracted proteins: 0\n'
+               'Task completed.\n')
+        self.assertEqual(res.output, exp)
+
+        # extract proteins
+        params = ['--infile', self.input_faa,
+                  '--outfile', join(self.working_dir, 'output.faa'),
+                  '--identifiers', '1,3',
+                  '--represent', None]
+        res = CliRunner().invoke(_process_fasta, params)
+        self.assertEqual(res.exit_code, 0)
+        exp = ('Number of extracted proteins: 2\n'
+               'Task completed.\n')
+        self.assertEqual(res.output, exp)
+
+    def tearDown(self):
+        rmtree(self.working_dir)
+
+
+if __name__ == '__main__':
+    main()
