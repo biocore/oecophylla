80dbb6c6a4bce6f71fe74d8475f0d13be0e76d6c
tkosciol <tkosciolek@ucsd.edu>
2017-09-14 14:12:36 -0700

    updates to launch script

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# runs setup of base environment and database installs
-
-# create core conda env
-conda env create --name oecophylla -f environment.yml
-
-find oecophylla -name "*.sh" -execdir bash {} \;
-
--- scripts/oecophylla.py
+++ scripts/oecophylla.py
@@ -1,11 +1,26 @@
 import click
 import json
 import os
+import glob
 
 """
 This is the main Oecophylla script which handles launching of the entire
-pipeline.
+pipeline and installation of all necessary modules/environments.
+Using a set of FASTA/FASTQ input files,
+sample sheet (Illumina-specific) and tool parameters file,
+it launches the Snakemake pipeline, either locally or on a supported
+cluster system (Torque, Slurm).
 
+Example usage:
+==============
+
+Installation:
+-------------
+oecophylla install
+
+Execute Workflow:
+-----------------
+oecophylla workflow --input-dir ./inputs --sample-sheet sample.txt --params params.json --output-dir ./outputs
 
 """
 
@@ -20,6 +35,11 @@ def _arg_split(ctx, param, value):
     _files = [c.strip() for c in value.split(',')]
     return _files
 
+
+def _create_dir(_path):
+    if not os.path.exists(_path):
+        os.makedirs(_path)
+
 @run.command()
 @click.option('--input-dir', '-i', required=True, type=click.STRING,
               callback=_arg_split,
@@ -28,7 +48,7 @@ def _arg_split(ctx, param, value):
               callback=_arg_split,
               help='Sample sheets used to demultiplex the Illumina run.')
 @click.option('--params', '-p', type=click.PATH, required=True,
-              help='Specify parameters for the tools.')
+              help='Specify parameters for the tools in a JSON file.')
 @click.option('--cluster-params', type=click.PATH, required=False,
               help='File with parameters for a cluster job.')
 @click.option('--local-scratch', type=click.PATH, default='/tmp'
@@ -48,30 +68,39 @@ def _arg_split(ctx, param, value):
 # TODO: everything past here will be passed directly into snakemake
 def workflow():
     import snakemake
-    from skbio.io.registry import Format
+    from skbio.io.registry import sniff
 
     # INPUT DIR
-    # sniff with scikit bio
+    for inp_file in glob.glob('%s/*' % input_dir):
+        file_format = sniff(inp_file)[0]
+        if (file_format != 'fasta') or (file_format != 'fastq'):
+            raise TypeError('Input files need to be in FASTA or FASTQ format.')
 
-    # SAMPLE MANIFEST
+    # SAMPLE SHEET
     # If a manifest is not specified, the files containing forward and reverse reads
     # will be automatically identified using regex.
 
 
     # OUTPUT
+    # create output directory, if does not exist
+    _create_dir(output_dir)
+
+    # PARAMS
 
 
     # LOGS
     if log_dir:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        _create_dir(log_dir)
     else:
         os.makedirs('%s/%s' % (output_dir, 'cluster_logs'))
+
+    # CLUSTER SETUP
 
 
     snakefile = "%s/../Snakefile" % __file__
     with open(cluster_params) as _file:
         cluster = json.load(_file)
+
     # TODO cluster queue logic
 
     snakemake.snakemake(snakefile, listrules=False, list_target_rules=False,
@@ -97,8 +126,9 @@ def workflow():
 
 
 @run.command()
-@click.option('--config-file',
-              help='Input biom table of abundances (optional)')
+@click.option('--config-file', default=None,
+              help='Configuration file defining specific installations of all'
+                   ' modules.')
 def install():
     """ This replaces the install.sh
 
@@ -106,6 +136,8 @@ def install():
     This configuration file will be implicitly passed to the other
     commands.
     """
+    if not config_file:
+        import install
 
 if __name__ == "__main__":
     run()
