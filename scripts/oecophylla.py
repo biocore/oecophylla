6faa6a821f4166a8302bd1f55c72781e32c1bc50
tkosciol <tkosciolek@ucsd.edu>
2017-09-13 22:49:17 -0700

    launch script skeleton

--- scripts/oecophylla.py
+++ scripts/oecophylla.py
@@ -1,4 +1,23 @@
 import click
+try:
+    import snakemake
+except ImportError:
+    import subprocess
+    subprocess.call(["source activate", 'oecophylla'])
+    import snakemake
+"""
+notes
+-----
+
+we need to activate the environment first 
+
+expected functionality:
+  * torque submission
+  * slurm submission (flag --cluster-queue)
+  * local run (flag --cluster-queue local)
+
+  * option to generate only config files (flag)
+"""
 
 @click.group()
 def run():
@@ -6,7 +25,11 @@ def run():
 
 @run.command()
 @click.option('--config-file',
-              help='Input biom table of abundances')
+              help='Paths to databases')
+@click.option('--params-file',
+	      help='Tool parameters')
+@click.option('--manifest',
+              help='Paths to input files; preparation information.')
 def workflow():
     pass
 
