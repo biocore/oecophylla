7fb3b7ed2f9a1f125f3a27bfb4c9440c794ea96e
Daniel McDonald <mcdonadt@colorado.edu>
2017-09-14 11:14:33 -0700

    quite -> quiet

--- oecophylla/qc/qc.sh
+++ oecophylla/qc/qc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-conda env create --name oecophylla-qc -f oecophylla-qc.yaml --quite > /dev/null
+conda env create --name oecophylla-qc -f oecophylla-qc.yaml --quiet > /dev/null
