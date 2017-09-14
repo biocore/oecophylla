16431d311f75ca283f7b90c4e4befec50e7d0242
Antonio Gonzalez <antgonza@gmail.com>
2017-09-14 12:54:14 -0600

    echo remove

--- uninstall-conda-envs.sh
+++ uninstall-conda-envs.sh
@@ -2,5 +2,7 @@
 
 # deletes all oecophylla conda environments
 for e in `conda-env list | grep oecophylla | awk '{print $1}'`; do
+  echo "removing conda env $e"
+  source deactivate
   conda-env remove --quiet --yes --name $e > /dev/null ;
 done
