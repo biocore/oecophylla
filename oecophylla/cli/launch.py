2a78e6538a1f620722c6c6210ba4e3e1bb17983b
Jamie Morton <jamietmorton@gmail.com>
2017-09-15 09:25:01 -0700

    Adding in working config generation file

--- oecophylla/cli/launch.py
+++ oecophylla/cli/launch.py
@@ -49,7 +49,7 @@ def _create_dir(_path):
 
 
 @run.command()
-@click.argument('targets', nargs=-1)
+@click.argument('targets', nargs=1)
 #TODO add an option to process multiple directories
 @click.option('--input-dir', '-i', required=True, type=click.STRING,
               help='Input directory with all of the samples.')
@@ -61,7 +61,7 @@ def _create_dir(_path):
               help='Specify parameters for the tools in a YAML file.')
 @click.option('--envs', '-e', type=click.Path(exists=True), required=True,
               help='Specify environments for the tools in a YAML file.')
-@click.option('--cluster-params', type=click.Path(), required=False,
+@click.option('--cluster-config', type=click.Path(), required=False,
               help='File with parameters for a cluster job.')
 @click.option('--local-scratch', type=click.Path(), default='/tmp',
               help='Temporary directory for storing intermediate files.')
@@ -76,10 +76,11 @@ def _create_dir(_path):
                     '(needs to be passed in by quotes)'))
 @click.option('--force', is_flag=True, default=False,
               help='Restarts the run and overwrites previous input.')
+@click.option('--just-config', is_flag=True, default=False,
+              help='Only generate the configuration file.')
 def workflow(targets, input_dir, sample_sheet, params, envs,
              cluster_config,local_scratch, workflow_type, output_dir,
-             snakemake_args, force):
-    print('hello')
+             snakemake_args, force, just_config):
     import snakemake
     from skbio.io.registry import sniff
 
@@ -106,41 +107,40 @@ def workflow(targets, input_dir, sample_sheet, params, envs,
         sample_dict = extract_samples_from_sample_sheet(_sheet, input_dir)
     else:
         sample_dict = extract_sample_paths(input_dir)
-    print(sample_dict)
+
     # PARAMS
     with open(params, 'r') as f:
         params_dict = yaml.load(f)
-    print(params_dict)
+
     # ENVS
     with open(envs, 'r') as f:
         envs_dict = yaml.load(f)
-    print(envs_dict)
+
     # CONFIG
     # merge PARAMS, SAMPLE_DICT, ENVS
     config_dict = {}
     config_dict['samples'] = sample_dict
     config_dict['params'] = params_dict
     config_dict['envs'] = envs_dict
-    print(config_dict)
+
     config_yaml = yaml.dump(config_dict, default_flow_style=False)
     config_fp = '%s/%s' % (local_scratch, 'config.yaml')
     with open(config_fp, 'w') as f:
         f.write(config_yaml)
-    print(config_fp)
-    print(config_yaml)
 
-    # LOGS
-    if log_dir:
-        _create_dir(log_dir)
-    else:
-        os.makedirs('%s/%s' % (output_dir, 'cluster_logs'))
-
-    # CLUSTER SETUP
-    with open(cluster_config) as _file:
-        _cluster_config = yaml.load(_file)
-    # for now, everything under `extra` should be explicit freetext,
-    # e.g. --my-argument=value
-    cluster_freetext = _cluster_config['extra']
+    # TODO: LOGS
+    # if log_dir:
+    #     _create_dir(log_dir)
+    # else:
+    #     os.makedirs('%s/%s' % (output_dir, 'cluster_logs'))
+    cluster = {}
+    if workflow_type == 'torque' or workflow_type == 'slurm':
+        # CLUSTER SETUP
+        with open(cluster_config) as _file:
+            cluster = yaml.load(_file)
+        # for now, everything under `extra` should be explicit freetext,
+        # e.g. --my-argument=value
+        cluster_freetext = _cluster_config['extra']
     if workflow_type == 'torque':
         cluster_setup = "qsub -e {cluster.error} -o {cluster.output} \
                          -m {cluster.email} \
@@ -187,8 +187,9 @@ def workflow(targets, input_dir, sample_sheet, params, envs,
     else:
         raise ValueError('Incorrect run-location specified in launch script.')
 
-    proc = subprocess.Popen(cmd, shell=True)
-    proc.wait()
+    if just_config:
+        proc = subprocess.Popen(cmd, shell=True)
+        proc.wait()
 
 
 @run.command()
