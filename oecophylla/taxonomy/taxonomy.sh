dc643d70df3b41512bb16a6df0ada0a60458e19f
qiyunzhu <qiyunzhu@gmail.com>
2017-09-15 17:23:58 -0700

    added SHOGUN support

--- oecophylla/taxonomy/taxonomy.sh
+++ oecophylla/taxonomy/taxonomy.sh
@@ -7,37 +7,27 @@ set -e
 
 conda env create --name oecophylla-kraken -f oecophylla-kraken.yaml --quiet > /dev/null
 
-# comment out all old shogun installation scripts
-
 # currently shogun is a hack, running the install script until we
 # have stable conda install
-# conda env create --name oecophylla-shogun -f oecophylla-shogun.yaml --quiet > /dev/null
+conda env create --name oecophylla-shogun -f oecophylla-shogun.yaml --quiet > /dev/null
 
-# source activate oecophylla-shogun
+source activate oecophylla-shogun
 
-# echo $CONDA_PREFIX
+echo $CONDA_PREFIX
 
-# mkdir -p $CONDA_PREFIX/etc/conda/activate.d
-# mkdir -p $CONDA_PREFIX/etc/conda/deactivate.d
+mkdir -p $CONDA_PREFIX/etc/conda/activate.d
+mkdir -p $CONDA_PREFIX/etc/conda/deactivate.d
 
-# touch $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-# touch $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
+touch $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+touch $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
 
-# echo "#!/bin/sh" >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-# echo "export OLD_PATH=$PATH" >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-# echo "export PATH=$PATH:$PWD/utree" >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+# path to the pre-compiled binaries on Barnacle
+bindir=/projects/ag500/shogun/bin
 
+echo '#!/bin/sh' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+echo "export OLD_PATH=$PATH" >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
+echo "export PATH=$PATH:$bindir" >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 
-# echo "#!/bin/sh" >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
-# echo "export PATH=$OLD_PATH" >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
-# echo "unset OLDPATH" >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
-
-
-# download UTree binary and add to path
-# if hash wget 2>/dev/null; then
-#     wget --quiet https://github.com/knights-lab/UTree/releases/download/v1.2/utree_1.2_linux.zip
-# else
-#     curl -s -L -o utree_1.2_linux.zip https://github.com/knights-lab/UTree/releases/download/v1.2/utree_1.2_linux.zip
-# fi
-# unzip utree_1.2_linux.zip -d utree > /dev/null
-# chmod 755 utree/utree*
+echo '#!/bin/sh' >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
+echo "export PATH=$OLD_PATH" >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
+echo "unset OLDPATH" >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
