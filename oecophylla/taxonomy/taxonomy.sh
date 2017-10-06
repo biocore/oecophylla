7da49a1798d9ff834beb0a6a446c907363a393af
Jon Sanders <jsanders@oeb.harvard.edu>
2017-10-06 15:29:14 -0400

    updated taxonomy install for portable SHOGUN

--- oecophylla/taxonomy/taxonomy.sh
+++ oecophylla/taxonomy/taxonomy.sh
@@ -3,7 +3,7 @@ set -e
 
 # the MetaPhlAn package contains the whole database, which is huge
 # uncomment only when tests are necessary
-# conda env create --name oecophylla-metaphlan2 -f oecophylla-metaphlan2.yaml --quiet > /dev/null
+conda env create --name oecophylla-metaphlan2 -f oecophylla-metaphlan2.yaml --quiet > /dev/null
 
 conda env create --name oecophylla-kraken -f oecophylla-kraken.yaml --quiet > /dev/null
 
@@ -17,19 +17,10 @@ source activate oecophylla-shogun
 
 echo $CONDA_PREFIX
 
-mkdir -p $CONDA_PREFIX/etc/conda/activate.d
-mkdir -p $CONDA_PREFIX/etc/conda/deactivate.d
+cd $CONDA_PREFIX/bin
 
-touch $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-touch $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
+wget https://github.com/knights-lab/UTree/releases/download/v2.0c/utree-search_gg
+chmod 755 utree-search_gg
 
-# path to the pre-compiled binaries on Barnacle
-bindir=/projects/genome_annotation/shogun/bin
-
-echo '#!/bin/sh' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-echo "export OLD_PATH=\$PATH" >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-echo "export PATH=\$PATH:${bindir}" >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
-
-echo '#!/bin/sh' >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
-echo "export PATH=\$OLD_PATH" >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
-echo "unset OLDPATH" >> $CONDA_PREFIX/etc/conda/deactivate.d/env_vars.sh
+wget https://github.com/knights-lab/BURST/releases/download/v0.99.4a/burst_linux_DB15
+chmod 755 burst_linux_DB15
