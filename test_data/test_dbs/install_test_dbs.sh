dc70dc0c1d96d84ef3288c4257053e814984ceca
Jon Sanders <jsanders@oeb.harvard.edu>
2017-10-06 15:28:55 -0400

    updated test db locations in ymls

--- test_data/test_dbs/install_test_dbs.sh
+++ test_data/test_dbs/install_test_dbs.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+# unzip bracket test
+tar -xjf bracken.tar.bz2
+
 # unzip bowtie2 test
 tar -xzf bowtie2.tar.gz
 
 # unzip centrifuge test
-tar -xzf centrifuge_test_db.tar.gz
+tar -xjf centrifuge.tar.bz2
 
 # unzip chocophlan test
 tar -xzf chocophlan_test.tar.gz
 
 # unzip kraken test
-tar -xzf kraken_test_db.tar.gz
+tar -xjf kraken.tar.bz2
 
 # unzip MetaPhlAn2 test
-tar -xzf mp2_test_db.tar.gz
+tar -xzf mp2_mini.tar.gz
 
 # unzip SHOGUN test
-tar -xzf shogun_test_db.tar.gz
+tar -xjf shogun.tar.bz2
 
 # unzip HUMAnN2 test
 tar -xzf uniref50_mini.tar.gz
