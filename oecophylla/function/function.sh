79fd6a72d2d8ac95f2eeb799c3c36b4bf8250542
qiyunzhu <qiyunzhu@gmail.com>
2017-10-09 15:04:07 -0700

    fixed shogun install code

--- oecophylla/function/function.sh
+++ oecophylla/function/function.sh
@@ -3,7 +3,7 @@ set -e
 
 conda env create --name oecophylla-humann2 -f oecophylla-humann2.yaml --quiet > /dev/null
 
-if [[ conda env list | grep '^oecophylla-shogun\s' ]]
+if conda env list | grep -q '^oecophylla-shogun\s'
 then
-  conda env create --name oecophylla-humann2 -f ../taxonomy/oecophylla-shogun.yaml --quiet > /dev/null
+  conda env create --name oecophylla-shogun -f ../taxonomy/oecophylla-shogun.yaml --quiet > /dev/null
 fi
