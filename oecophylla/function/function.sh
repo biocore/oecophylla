0418a655cd9308395c6c88e54ef83aed95d1264e
qiyunzhu <qiyunzhu@gmail.com>
2017-10-09 15:06:44 -0700

    fixed shogun install code 2

--- oecophylla/function/function.sh
+++ oecophylla/function/function.sh
@@ -3,7 +3,8 @@ set -e
 
 conda env create --name oecophylla-humann2 -f oecophylla-humann2.yaml --quiet > /dev/null
 
-if conda env list | grep -q '^oecophylla-shogun\s'
+# Create the SHOGUN environment only if it hasn't already been created by the taxonomy module.
+if ! conda env list | grep -q '^oecophylla-shogun\s'
 then
   conda env create --name oecophylla-shogun -f ../taxonomy/oecophylla-shogun.yaml --quiet > /dev/null
 fi
