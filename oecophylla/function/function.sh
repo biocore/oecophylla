afb6fb086047c5d536dd52f900515cf42f6667bd
qiyunzhu <qiyunzhu@gmail.com>
2017-10-09 12:45:56 -0700

    added SHOGUN functional profiling

--- oecophylla/function/function.sh
+++ oecophylla/function/function.sh
@@ -2,3 +2,8 @@
 set -e
 
 conda env create --name oecophylla-humann2 -f oecophylla-humann2.yaml --quiet > /dev/null
+
+if [[ conda env list | grep '^oecophylla-shogun\s' ]]
+then
+  conda env create --name oecophylla-humann2 -f ../taxonomy/oecophylla-shogun.yaml --quiet > /dev/null
+fi
